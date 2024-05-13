4c6917abcba635e1757a209106088a0bbd68d671
Aaryaman-Bhardwaj <->
2024-05-13 19:30:13 +0530

    Hello Controller created

---
+++ src/main/java/com/attainx/uscisspringbootdemo/UscisSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.attainx.uscisspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UscisSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UscisSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attainx/uscisspringbootdemo/UscisSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attainx.uscisspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UscisSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
